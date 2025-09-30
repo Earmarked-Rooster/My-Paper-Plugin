814823eec0690afd778744c9b3bd347c8b522816
Earmarked-Rooster <rio4tahmazyan@gmail.com>
2025-09-30 10:59:46 -0700

    Update CustomEnchantment.java for comments

--- src/main/java/io/github/EarmarkedRooster/examplePluginWithMinecraft/enchantments/CustomEnchantment.java
+++ src/main/java/io/github/EarmarkedRooster/examplePluginWithMinecraft/enchantments/CustomEnchantment.java
@@ -1,7 +1,7 @@
 package io.github.EarmarkedRooster.examplePluginWithMinecraft.enchantments;
 
 import org.bukkit.enchantments.Enchantment;
-
+// TO BE DELETED - ENCHANTMENTS ARE NOT CUSTOMIZED THIS WAY VERSIONS 1.21 +
 public interface CustomEnchantment {
     void register();
     Enchantment getEnchantment();
