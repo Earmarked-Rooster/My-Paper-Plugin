97533efbff9da9ae63e33abe0855f68d17d60e2f
Earmarked-Rooster <rio4tahmazyan@gmail.com>
2025-09-30 11:04:43 -0700

    Update FactionPowerListener.java for comments

--- src/main/java/io/github/EarmarkedRooster/examplePluginWithMinecraft/listeners/FactionPowerListener.java
+++ src/main/java/io/github/EarmarkedRooster/examplePluginWithMinecraft/listeners/FactionPowerListener.java
@@ -9,7 +9,7 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 
 public class FactionPowerListener implements Listener {
 
-    private static final int FACTIONPOWERSENTINEL = 10;
+    private static final int FACTIONPOWERCONSTANT = 10;
     private final FactionDatabase factionDatabase;
     private final SocialCreditDatabase socialCreditDatabase;
 
@@ -23,23 +23,27 @@ public class FactionPowerListener implements Listener {
         if (event.getEntity().getKiller() != null) {
             Player killer = event.getEntity().getKiller();
             Player victim = event.getEntity();
-
+            // grabs the killer and victim of the killer for this code to function
             String killerFaction = factionDatabase.getPlayerFaction(killer.getUniqueId());
             String victimFaction = factionDatabase.getPlayerFaction(victim.getUniqueId());
-
+            
             if (killerFaction != null && victimFaction != null && !killerFaction.equals(victimFaction)) {
+                // increases faction power for killer's faction
                 int factionPower = factionDatabase.getFactionPower(killerFaction);
-                factionDatabase.setFactionPower(killerFaction, factionPower + FACTIONPOWERSENTINEL);
+                factionDatabase.setFactionPower(killerFaction, factionPower + FACTIONPOWERCONSTANT);
 
+                // increases social credit score for killer
                 int currentScore = socialCreditDatabase.getSocialCreditScores().getOrDefault(killer.getUniqueId(), 0);
-                int newScore = currentScore + FACTIONPOWERSENTINEL;
+                int newScore = currentScore + FACTIONPOWERCONSTANT;
                 socialCreditDatabase.getSocialCreditScores().put(killer.getUniqueId(), newScore);
 
+                // decreases faction power for victim's faction
                 int victimFactionPower = factionDatabase.getFactionPower(victimFaction);
-                factionDatabase.setFactionPower(victimFaction, victimFactionPower - FACTIONPOWERSENTINEL);
+                factionDatabase.setFactionPower(victimFaction, victimFactionPower - FACTIONPOWERCONSTANT);
 
+                // decreases social credit for victim
                 int victimCurrentScore = socialCreditDatabase.getSocialCreditScores().getOrDefault(victim.getUniqueId(), 0);
-                socialCreditDatabase.getSocialCreditScores().put(victim.getUniqueId(), victimCurrentScore - FACTIONPOWERSENTINEL);
+                socialCreditDatabase.getSocialCreditScores().put(victim.getUniqueId(), victimCurrentScore - FACTIONPOWERCONSTANT);
             }
         }
     }
